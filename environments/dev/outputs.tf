4853a5582f7608c9c8cdf45ab9bd685478ed0574
jayeshjeh <jay.parmar.11169@gmail.com>
2025-05-05 19:35:20 +0530

    outputs.tf for dev env

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -10,14 +10,11 @@ output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
 
-
 output "security_group_id" {
-  value = module.security_group.security_group_id
+  value = module.security_group.SG_id
 }
 
-
 output "sqs_queue_url" {
-
   value = module.sqs.queue_url
 }
 
@@ -25,17 +22,17 @@ output "sqs_queue_arn" {
   value = module.sqs.queue_arn
 }
 
-
 output "lambda_role_arn" {
-
   value = module.iam.lambda_role_arn
 }
 
-output "github_actions_role_arn" {
-  description = "The ARN of the GitHub Actions OIDC role"
-  value       = aws_iam_role.github_actions.arn
+output "sqs_read_policy_arn" {
+  value = module.iam.sqs_read_policy_arn
 }
 
+output "cloudwatch_logs_policy_arn" {
+  value = module.iam.cw_logs_policy_arn
+}
 
 output "lambda_function_arn" {
   value = module.lambda.lambda_function_arn
@@ -45,12 +42,14 @@ output "lambda_function_name" {
   value = module.lambda.lambda_function_name
 }
 
-
-output "event_mapping_id" {
-  value = module.event_mapping.event_mapping_id
+output "event_mapping_uuid" {
+  value = module.event_mapping.mapping_uuid
 }
 
 output "github_oidc_provider_arn" {
-
   value = aws_iam_openid_connect_provider.github.arn
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
 }
