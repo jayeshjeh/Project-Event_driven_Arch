857328966194316bb2e8920b4f9725bd8a3c592b
jayeshjeh <jay.parmar.11169@gmail.com>
2025-05-05 18:20:46 +0530

    lambda, variable, error solved

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,7 +1,7 @@
 variable "function_name" {}
 variable "lambda_role_arn" {}
 variable "handler" {
-  default = index.handler
+  default = "index.handler"
 }
 variable "runtime" {}
 variable "lambda_package" {}
