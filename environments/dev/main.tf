a5e61985d0cfbf93f17be12306677a1005de1362
jayeshjeh <jay.parmar.11169@gmail.com>
2025-05-05 18:13:24 +0530

    Github OIDC

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -65,3 +65,50 @@ module "event_mapping" {
   depends_on_lambda   = module.lambda_function_arn
   depends_on_sqs      = module.sqs_queue_arn
 }
+
+
+# OIDC
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"  # GitHub's OIDC thumbprint
+  ]
+}
+
+# IAM Role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+  name = "${var.name_prefix}-github-oidc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          },
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:jayeshjeh/Project-Event_driven_Arch:ref:refs/heads/main"
+
+          }
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+# Attach policy to allow Terraform operations
+resource "aws_iam_role_policy_attachment" "github_terraform_attach" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"  
+}
